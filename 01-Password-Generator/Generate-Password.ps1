e498b610a5de6b181f198f9ba9b5467e76361533
Adriano Felix Lacerda <105937554+Adriano10Lacerda@users.noreply.github.com>
2026-08-07 16:01:36 -0300

    Add initial password generator script

--- 01-Password-Generator/Generate-Password.ps1
+++ 01-Password-Generator/Generate-Password.ps1
@@ -10,10 +10,18 @@ $passwordLenght = Read-Host "Enter password Length"
 
 Write-Host ""
 
-Write-Host "Selected Length: $PasswordLength" -Foreground Yellow
+Write-Host "Selected Length: $PasswordLength" -ForegroundColor Yellow
 
 Clear-Host
 
 Write-Host "------------------------------------" -ForegroundColor Cyan
 Write-Host "    Enterprise Password Generator  " -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Cyan
+
+Write-Host ""
+
+$PasswordLength - Read-Host "Enter Password Length"
+
+Write-Host ""
+
+Write-Host "Selected Length" $PasswordLength" -ForegroundColor Yellow
